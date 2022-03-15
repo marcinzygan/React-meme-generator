60e158a3cdc80192904246b7cb21f6c9baf9d737
marcinzygan <soul@o2.pl>
2022-03-15 22:22:59 +0000

    iconify package

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -4,21 +4,18 @@ import { Icon } from '@iconify/react';
 
 
 export default function Meme(){
-    // console.log(memedata.data.memes);
-    // const memeImg = memedata.data.memes.map(item => 
-    //     <h1>{item.url}</h1>
-    // )
+   
     let randomImg
     function getMemeImg(){
         console.log("clicked");
         const memesArray = memedata.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length);
         randomImg = memesArray[randomNumber].url
-        console.log(randomImg);
-        
-
-        
+        console.log(randomImg);    
     }
+
+
+
     return(
     <div className="meme__container">
         <div className="form">
